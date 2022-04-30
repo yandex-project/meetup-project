f35b9e926d7d54185219f4c4a6082bba81253f73
yeezy_na_izi <howone1sky@gmail.com>
2022-04-30 15:24:42 +0300

    create homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def homepage(request):
+    template_name = 'homepage/index.html'
+    context = {}
+
+    return render(request, template_name, context)
--- meetup_project/urls.py
+++ meetup_project/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('homepage.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
