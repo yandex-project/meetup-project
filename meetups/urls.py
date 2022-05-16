409b6bd5848df958e3d0ea4e7e88e6f9fc87d6b1
yeezy_na_izi <howone1sky@gmail.com>
2022-05-16 11:30:44 +0300

    Meetups: [~] login required on CreateMeetup page

--- meetups/urls.py
+++ meetups/urls.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from meetups import views
 
 urlpatterns = [
-    path('add_meetup/', views.CreateMeetupView.as_view(), name='create_meetup'),
+    path('add_meetup/', login_required(views.CreateMeetupView.as_view()), name='create_meetup'),
     path('<slug>/', views.MeetupDetailView.as_view(), name='meetup_detail'),
     path('<slug>/add_lecture/', views.CreateLectureView.as_view(), name='create_lecture'),
     path('<slug>/<pk>/', views.UpdateLectureView.as_view(), name='update_lecture'),
