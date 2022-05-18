acb7c75319354cc4650fd46344b78678c8dd2ac9
Andrey <andrey.krupskiy.1992@gmail.com>
2022-05-18 18:02:14 +0300

    sql requests amount reduced

--- schedule/utils.py
+++ schedule/utils.py
@@ -10,6 +10,7 @@ class Calendar(HTMLCalendar):
     def __init__(self, year=None, month=None):
         self.year = year
         self.month = month
+        self.meetups = []
         super(Calendar, self).__init__()
 
     def formatweekday(self, day):
@@ -31,27 +32,30 @@ class Calendar(HTMLCalendar):
             self.cssclass_month_head, s)
 
     # formats a day as a td
-    def formatday(self, day, meetups):
-        meetups_per_day = meetups.filter(date__day=day)
+    def formatday(self, day):
+        meetups = self.meetups[day]
         data = ""
-        for event in meetups_per_day:
-            data += f"<li> {event.get_html_url} </li>"
+        for meetup in meetups:
+            data += f"<li> {meetup} </li>"
         if day != 0:
             return f"<td><span class='date'>{day}</span><ul> {data} </ul></td>"
         return "<td></td>"
 
     # formats a week as a tr
-    def formatweek(self, theweek, meetup=None):
+    def formatweek(self, theweek):
         week = ""
         for data, weekday in theweek:
-            week += self.formatday(data, meetup)
+            week += self.formatday(data)
         return f"<tr> {week} </tr>"
 
     # formats a month as a table
     def formatmonth(self, withyear=True, **kwargs):
         meetups = Meetup.objects.filter(
             date__year=self.year, date__month=self.month
-        )
+        ).all()
+        self.meetups = [[] for i in range(32)]
+        for meetup in meetups:
+            self.meetups[meetup.date.day].append(meetup.get_html_url)
         calendar = (
             '<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
         )
@@ -60,6 +64,6 @@ class Calendar(HTMLCalendar):
         )
         calendar += f"{self.formatweekheader()}\n"
         for week in self.monthdays2calendar(self.year, self.month):
-            calendar += f"{self.formatweek(week, meetups)}\n"
+            calendar += f"{self.formatweek(week)}\n"
         calendar += '</table>'
         return calendar
