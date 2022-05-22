95e2b990c183398b9b8420ed5293593dc3bc147f
yeezy_na_izi <howone1sky@gmail.com>
2022-05-22 20:03:14 +0300

    Flake8: [~] fix troubles

--- maps/models.py
+++ maps/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
 from meetups.models import Meetup
 from djeym.models import Placemark
-from maps.utils import address_to_coords
+# from maps.utils import address_to_coords
 
 
 class Marker(models.Model):
