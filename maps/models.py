10281923f001a1f975a64de17d7c9d92086be6a1
yeezy_na_izi <howone1sky@gmail.com>
2022-05-22 20:01:31 +0300

    Requirements: [+] yandex_geocoder
    Maps:
    [~] comment receiver (it does not work((()

--- maps/models.py
+++ maps/models.py
@@ -24,22 +24,22 @@ class Marker(models.Model):
         return f'Marker for meetup {self.meetup}'
 
 
-@receiver(post_save, sender=Meetup)
-def create_meetup_placemark(sender, instance, created, **kwargs):
-    if created:
-        base = Placemark.objects.get(header='base')
-        mark = Placemark.objects.create(
-                                        ymap=base.ymap,
-                                        category=base.category,
-                                        header=instance.name,
-                                        body=instance.description,
-                                        footer=instance.date,
-                                        coordinates=address_to_coords(instance.place),
-                                        icon_slug=base.icon_slug
-        )
-        Marker.objects.create(meetup=instance, placemark=mark)
-
-
-@receiver(post_save, sender=Meetup)
-def save_meetup_placemark(sender, instance, **kwargs):
-    instance.marker.save()
+# @receiver(post_save, sender=Meetup)
+# def create_meetup_placemark(sender, instance, created, **kwargs):
+#     if created:
+#         base = Placemark.objects.get(header='base')
+#         mark = Placemark.objects.create(
+#                                         ymap=base.ymap,
+#                                         category=base.category,
+#                                         header=instance.name,
+#                                         body=instance.description,
+#                                         footer=instance.date,
+#                                         coordinates=address_to_coords(instance.place),
+#                                         icon_slug=base.icon_slug
+#         )
+#         Marker.objects.create(meetup=instance, placemark=mark)
+#
+#
+# @receiver(post_save, sender=Meetup)
+# def save_meetup_placemark(sender, instance, **kwargs):
+#     instance.marker.save()
